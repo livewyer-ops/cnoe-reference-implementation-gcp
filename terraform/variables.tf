f838044f2860e1baadae9e84ec78f96d42d2cb4b
Bowen Sun <bowensun@gmail.com>
2025-01-20 16:19:35 +0000

    refactor: update codebase for GCP

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,29 +5,41 @@ variable "repo_url" {
 }
 
 variable "tags" {
-    description = "Tags to apply to AWS resources"
-    default = {
-        env = "dev"
-        project = "cnoe"
-    }
-    type = map(string)
+  description = "Tags to apply to GKE resources"
+  default = {
+    env     = "dev"
+    project = "cnoe"
+  }
+  type = map(string)
+}
+
+variable "org_id" {
+  description = "GCP Org ID"
+  type        = string
+  default     = ""
 }
 
 variable "region" {
-  description = "Region"
+  description = "GCP Region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east1"
+}
+
+variable "project" {
+  description = "GCP Project Name"
+  type        = string
+  default     = ""
 }
 
 variable "cluster_name" {
-  description = "EKS Cluster name"
+  description = "GKE Cluster name"
   default     = "cnoe-ref-impl"
   type        = string
 }
 
 variable "hosted_zone_id" {
   description = "If using external DNS, specify the Route53 hosted zone ID. Required if enable_dns_management is set to true."
-  default     = "Z0202147IFM0KVTW2P35"
+  default     = ""
   type        = string
 }
 
